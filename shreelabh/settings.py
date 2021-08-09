45f644d201365759da6a207ece7fc73122d77c3e
kunjalsavaliya <ksavaliya1470@gmail.com>
2021-08-09 18:50:18 +0530

    updated migration option

--- shreelabh/settings.py
+++ shreelabh/settings.py
@@ -79,6 +79,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+         'OPTIONS': {
+            'init_command': 'SET innodb_strict_mode=1',
+        },
     }
 }
 
